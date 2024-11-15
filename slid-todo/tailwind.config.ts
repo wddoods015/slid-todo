0d5c62697c9e38beadc86ea12049c1235bce4e11
catmaker <rzt0317@nate.com>
2024-11-15 18:03:53 +0900

    style: 폰트 설정
    - Pretendard 폰트 기본 설정 추가
    - font-medium을 기본 weight로 설정
    - antialiased 적용

--- slid-todo/tailwind.config.ts
+++ slid-todo/tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        "pretendard-light": ["Pretendard-Light"],
+        "pretendard-regular": ["Pretendard-Regular"],
+        "pretendard-medium": ["Pretendard-Medium"],
+        "pretendard-semibold": ["Pretendard-SemiBold"],
+        "pretendard-bold": ["Pretendard-Bold"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
