3608c9c96e06138b8e061653c27d9d5b88436a6b
catmaker <rzt0317@nate.com>
2024-11-18 00:39:30 +0900

    chore: 빌드 시 린트 검사 비활성화
    - next.config.js에 eslint.ignoreDuringBuilds 옵션 추가
    - 빌드 프로세스 최적화

--- slid-todo/next.config.js
+++ slid-todo/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  prettier: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
