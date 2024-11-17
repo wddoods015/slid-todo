a2eb27edb2489069f68b57aab9c0a2b7d74329c6
신지수 <114233139+catmaker@users.noreply.github.com>
2024-11-18 00:29:28 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 cd ../
 mkdir output
-# node_modules를 제외한 모든 파일 복사
-cp -R ./code-it-2/* ./output/       # 일반 파일들
-cp -R ./code-it-2/.[!.]* ./output/  # 숨김 파일들 (.storybook, .gitignore 등)
-rm -rf ./output/node_modules        # node_modules 제거
+cp -R ./code-it-2/* ./output
 cp -R ./output ./code-it-2/
