4a8d52eff571d2ab9e251f27ec62a3e4c94f9931
s-ggul <k0s0a7@gmail.com>
2024-12-06 17:50:09 +0900

    [design] embed 다크모드 구분

--- slid-todo/src/components/shared/note-viewer/embed-content.tsx
+++ slid-todo/src/components/shared/note-viewer/embed-content.tsx
@@ -18,7 +18,7 @@ const EmbedContent = ({ url, isVisible }: EmbedContentProps) => {
 
   return (
     <SheetPortal>
-      <div className="absolute lg:top-0 lg:right-[800px] lg:-mr-4 lg:w-[1000px] lg:h-full bg-gray-900 z-[51]">
+      <div className="absolute lg:top-0 lg:right-[800px] lg:-mr-4 lg:w-[1000px] lg:h-full bg-blue-50 dark:bg-gray-900 z-[51]">
         <div className="relative w-full h-full flex justify-center items-center">
           {error ? (
             <div className="text-white">이 사이트는 임베드할 수 없습니다.</div>
